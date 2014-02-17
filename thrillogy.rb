704635b66c9b6b1a59eeae0b683f880086b52e05
Ben Christel <bchristel@groupon.com>
2014-02-16 16:23:43 -0800

    initial implementation and tests

---
+++ thrillogy.rb
@@ -0,0 +1,165 @@
+module Thrillogy
+  class Hooks
+    @@warning_logger = nil
+    
+    attr_accessor :call
+    
+    public
+    def around
+      run
+    end
+    
+    public
+    %w(before after on_exception ensure).each do |meth|
+      define_method meth do; end
+    end
+    
+    %w(run receiver method_name arguments block returned exception options).
+    each do |meth|
+      define_method meth do; call.send meth end
+    end
+    
+    public
+    def self.on(_class, *methods)
+      Installer.new(_class, self).install(*methods)
+    end
+    
+    class << self
+      [:to, :for, :from, :at, :when].each do |_alias|
+        alias_method _alias, :on
+      end
+    end
+    
+    public
+    def self.warning_logger new_warning_logger=@@warning_logger
+      @@warning_logger = new_warning_logger
+    end
+    
+    public
+    def self.warn(msg)
+      warning_logger && warning_logger.warn(msg)
+    end
+    
+    class DefaultWarningLogger
+      def warn msg
+        $stderr.puts "WARNING: #{msg}"
+      end
+    end
+    
+    warning_logger DefaultWarningLogger.new
+  end
+  
+  class Installer
+    @@method_seqno = 0
+    
+    attr_accessor :target, :source
+    
+    def initialize(target, source)
+      self.target = target
+      self.source = source
+    end
+    
+    def install(*methods_and_options)
+      methods, options = separate(methods_and_options)
+      methods.each do |method_name|
+        _target = target # get attrs into local vars so it will be enclosed by
+        _source = source # the block passed to define_method
+        
+        aliased = uniquely_rename(method_name)
+        target.send :alias_method, aliased, method_name
+        target.send :define_method, method_name do |*args, &block|
+          MethodCall.new(
+            :delegate => _source.new(),
+            :receiver => self,
+            :method_name => method_name,
+            :aliased_method_name => aliased,
+            :arguments => args,
+            :block => block,
+            :options => options,
+          ).invoke
+        end
+      end
+    end
+    
+    def separate methods_and_options
+      options, methods = methods_and_options.partition { |x| x.is_a? Hash }
+      options = options.inject({}) { |merged, hash| merged.merge hash }
+      [methods, options]
+    end
+    
+    def uniquely_rename(name)
+      "_#{@@method_seqno += 1}_#{name}"
+    end
+  end
+  
+  class MethodCall
+    attr_accessor :receiver, :method_name, :aliased_method_name, :arguments,
+      :block, :returned, :exception, :options, :delegate, :run_called,
+      :call_finished, :current_callback
+      
+    public
+    [:returned, :exception].each do |attr|
+      define_method attr do
+        if not call_finished
+          warn "accessing `#{self.class}##{attr}` from #{current_callback}, where it is not yet set"
+        end
+        instance_variable_get "@#{attr}"
+      end
+    end
+    
+    def warn(*args)
+      delegate.class.warn(*args)
+    end
+    
+    public
+    [:before, :after, :around, :ensure, :on_exception].each do |callback|
+      lambda do
+        define_method callback do
+          self.current_callback = callback
+          delegate.send callback
+          self.current_callback = nil
+        end
+      end.call
+    end
+      
+    def initialize(params={})
+      params.each do |k, v|
+        self.send "#{k}=", v
+      end
+      self.run_called = false
+      self.call_finished = false
+      self.current_callback = nil
+    end
+    
+    def delegate= new_delegate
+      @delegate = new_delegate
+      new_delegate.call = self
+    end
+      
+    def run
+      self.run_called = true
+      before
+      begin
+        self.returned = receiver.send aliased_method_name, *arguments, &block
+      ensure
+        self.call_finished = true
+      end
+      after
+      returned
+    end
+    
+    def invoke
+      begin
+        around
+        raise "run not called" if not run_called
+        returned
+      rescue Exception => e
+        self.exception = e
+        on_exception
+        raise e
+      ensure
+        self.ensure
+      end
+    end
+  end
+end
---
+++ thrillogy_test.rb
@@ -0,0 +1,244 @@
+require 'thrillogy'
+
+class QueenElizabeth
+  @@annoyances = []
+  
+  def self.wills_that something_should_be_done
+    if block_given? and yield
+      praise something_should_be_done
+    else
+      scowl "We are not amused; #{something_should_be_done}!"
+    end
+  end
+  
+  def self.praise a_thing
+    print '.'
+  end
+  
+  def self.scowl something_wrong
+    print 'F'
+    @@annoyances << something_wrong
+  end
+  
+  def self.opine
+    puts ''
+    if @@annoyances.empty?
+      puts "*applause*"
+    else
+      puts @@annoyances
+    end
+  end
+end
+
+class Audience
+  @@memories = []
+  
+  class << self
+    def remember scene
+      @@memories << scene
+    end
+    
+    def recall
+      @@memories
+    end
+    
+    # returns true iff the audience recalls the scenes happening in the given
+    # order.
+    def recall? *scenes
+      unmatched = scenes.dup
+      @@memories.each do |memory|
+        unmatched.shift if unmatched[0] == memory
+      end
+      unmatched.empty?
+    end
+    
+    def forget
+      @@memories.clear
+    end
+    
+    def gasp
+      remember 'Gasp!'
+    end
+    
+    def react scene
+      remember 'Yay!' if scene == :happy
+      remember 'Boo!' if scene == :sappy
+    end
+  end
+end
+
+class Hamlet
+  def soliloquy(others_on_stage=nil)
+    raise 'totally goofed' if others_on_stage
+    "To be, or not to be?"
+  end
+  
+  def murder(*victims)
+    victims.each do |victim|
+      yield "Hamlet kills #{victim}!" if block_given?
+    end    
+  end
+  
+  def self.die
+    "Hamlet is dead!"
+  end
+end
+
+class WarningLogger
+  @@lines = []
+  
+  def self.warn line
+    @@lines << line
+  end
+  
+  def self.read
+    @@lines
+  end
+  
+  def self.clear
+    @@lines.clear
+  end
+end
+
+class InviteAudience < Thrillogy::Hooks
+  def around
+    Audience.gasp
+    run
+    Audience.react options[:feels]
+  end
+  
+  def before
+    event = "#{receiver.class} is about to do a #{method_name}"
+    event += " on #{arguments.join(' and ')}" unless arguments.empty?
+    Audience.remember event
+  end
+  
+  def after
+    if receiver.is_a? Module
+      rcv = "#{receiver}."
+    else
+      rcv = "#{receiver.class}#"
+    end
+    
+    Audience.remember "#{rcv}#{method_name} was like #{returned.inspect}"
+  end
+  
+  def on_exception
+    Audience.remember "#{receiver.class} #{exception}!"
+  end
+  
+  def ensure
+    Audience.remember "Hamlet had a soliloquy"
+  end
+  
+  warning_logger WarningLogger
+end
+
+InviteAudience.to Hamlet, :soliloquy, :feels => :happy
+InviteAudience.to Hamlet, :murder, :feels => :sappy
+class << Hamlet
+  InviteAudience.to self, :die
+end
+
+QueenElizabeth.wills_that "Hamlet's soliloquy should return the correct value" do
+  Audience.forget
+  Hamlet.new.soliloquy == "To be, or not to be?"
+end
+
+QueenElizabeth.wills_that "the audience should see Hamlet's soliloquy" do
+  Audience.forget
+  Hamlet.new.soliloquy
+  Audience.recall? "Hamlet had a soliloquy"
+end
+
+QueenElizabeth.wills_that "the audience should remember Hamlet's soliloquy even if he slips on a banana peel" do
+  Audience.forget
+  begin; Hamlet.new.soliloquy :banana_peel; rescue; end
+  Audience.recall? "Hamlet totally goofed!", "Hamlet had a soliloquy"
+end
+
+QueenElizabeth.wills_that "the audience should remember what Hamlet said" do
+  Audience.forget
+  Hamlet.new.soliloquy
+  Audience.recall? 'Hamlet#soliloquy was like "To be, or not to be?"'
+end
+
+QueenElizabeth.wills_that "the audience should not remember what Hamlet said if he slipped on a banana peel" do
+  Audience.forget
+  begin; Hamlet.new.soliloquy :banana_peel; rescue; end
+  not Audience.recall? 'Hamlet#soliloquy was like "To be, or not to be?"'
+end
+
+QueenElizabeth.wills_that "the audience should not remember Hamlet slipping on a banana peel if he didn't" do
+  Audience.forget
+  Hamlet.new.soliloquy
+  not Audience.recall? "Hamlet totally goofed!"
+end
+
+QueenElizabeth.wills_that "the audience should remember Hamlet killing people" do
+  Audience.forget
+  Hamlet.new.murder 'Dumbledore' do |event|
+    Audience.remember event
+  end
+  Audience.recall? 'Hamlet kills Dumbledore!'
+end
+
+QueenElizabeth.wills_that "the audience should anticipate Hamlet killing people" do
+  Audience.forget
+  Hamlet.new.murder 'Polonius', 'Laertes'
+  Audience.recall? 'Hamlet is about to do a murder on Polonius and Laertes'
+end
+
+QueenElizabeth.wills_that "the audience should cheer if Hamlet does a good thing but boo if he does a bad thing" do
+  Audience.forget
+  Hamlet.new.soliloquy
+  Hamlet.new.murder 'Polonius', 'Laertes'
+  Hamlet.die
+  Audience.recall? 'Yay!', 'Boo!'
+end
+
+QueenElizabeth.wills_that "the audience should anticipate and remember the soliloquy, in that order" do
+  Audience.forget
+  Hamlet.new.soliloquy
+  Audience.recall? 'Gasp!', 'Hamlet is about to do a soliloquy', 
+                   'Hamlet#soliloquy was like "To be, or not to be?"', 'Yay!'
+end
+
+QueenElizabeth.wills_that "the audience should remember Hamlet's self dying" do
+  Audience.forget
+  Hamlet.die
+  Audience.recall? 'Hamlet.die was like "Hamlet is dead!"'
+end
+
+QueenElizabeth.wills_that "we should be warned if we try to use information we don't have" do
+  Audience.forget
+  WarningLogger.clear
+  
+  class InviteAudience
+    def before
+      returned
+      exception
+    end
+    
+    def around
+      returned
+      exception
+      run
+    end
+  end
+  
+  Hamlet.new.soliloquy
+  
+  expected = [
+    'accessing `Thrillogy::MethodCall#returned` from before, where it is not yet set',
+    'accessing `Thrillogy::MethodCall#exception` from before, where it is not yet set',
+    'accessing `Thrillogy::MethodCall#returned` from around, where it is not yet set',
+    'accessing `Thrillogy::MethodCall#exception` from around, where it is not yet set'
+  ]
+  
+  expected.all? do |i|
+    WarningLogger.read.include? i
+  end
+end
+
+QueenElizabeth.opine
